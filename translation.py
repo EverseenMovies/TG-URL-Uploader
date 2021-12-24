8f0308c66276684d3b6505ae9ff2a42c81757267
EverseenMovies <96349268+EverseenMovies@users.noreply.github.com>
2021-12-24 14:21:00 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -7,8 +7,6 @@ This is a Telegram URL Upload Bot!
 /help for more details..
 
 Support Group : @Everseen_Movies"""
-    RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
-    ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  /help for Details"
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
@@ -45,7 +43,7 @@ Expires on: 31/12/2020"""
    Video  - Give File as video without Screenshots
    DFile  - Give File without Screenshots
    
-<b>👉 Create own Clone Bot :</b> 👉 <a href="https://youtu.be/QkAkSLBgoYw">Diploy</a>
+<b>👉 Create own Clone Bot :</b> 👉 <a href="Everseen_Movies">Diploy</a>
 
 --------
 Send /me to know current plan details
